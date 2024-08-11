dfa878483b9de15e4ca0f2512598c00036c698cb
RedCatHuu <148026462+RedCatHuu@users.noreply.github.com>
2024-08-11 15:52:37 +0900

    不要なコード削除

--- src/main/java/com/dmm/task/data/entity/Tasks.java
+++ src/main/java/com/dmm/task/data/entity/Tasks.java
@@ -6,13 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "tasks")
 public class Tasks {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
